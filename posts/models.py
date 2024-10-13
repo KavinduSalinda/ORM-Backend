5a252f552d3e0ab5da9de694bfbec5cb8e4f6d2f
KavinduSalinda <118047241+KavinduSalinda@users.noreply.github.com>
2024-10-13 13:32:29 +0530

    create Tag, postTag models

--- posts/models.py
+++ posts/models.py
@@ -11,3 +11,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.tag.name}"
